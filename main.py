d643fc70774044dc3618aa6b7e035445b55135d1
LeonardoLucaa <leonardolucab@gmail.com>
2023-12-23 17:03:38 -0300

    data-structure

---
+++ main.py
@@ -0,0 +1,11 @@
+# Funções
+cliente1 = [12,32,45,2,3,44]
+
+#Função sem parâmetro
+def falarOi():
+    print('Oi, seja bem vindo(a)')
+
+#Função com parâmetro que imprime string formatada
+def falarOiNome(nome):
+    print(f'Seja bem-vindo')
+print(soma)
